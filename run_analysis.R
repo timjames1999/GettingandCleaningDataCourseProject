a430ec160a7c846717247e3cff87455ee425db82
Tim James <121989305+timjames1999@users.noreply.github.com>
2023-03-05 21:31:09 -0500

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -26,12 +26,12 @@ activityLabels = read.table("./GettingandCleaningDataCourseProject/UCI HAR Datas
 
 #Assign variable names
 colnames(x_train) <- features[,2]
-colnames(y_train) <- "activityID"
-colnames(subject_train) <- "subjectID"
+colnames(y_train) <- "activityid"
+colnames(subject_train) <- "subjectid"
 colnames(x_test) <- features[,2]
-colnames(y_test) <- "activityID"
-colnames(subject_test) <- "subjectID"
-colnames(activityLabels) <- c("activityID", "activityType")
+colnames(y_test) <- "activityid"
+colnames(subject_test) <- "subjectid"
+colnames(activityLabels) <- c("activityid", "activityType")
 
 #Merge datasets into one
 alltrain <- cbind(y_train, subject_train, x_train)
@@ -44,8 +44,8 @@ finaldataset <- rbind(alltrain, alltest)
 colNames <- colnames(finaldataset)
 
 #Create vector for defining ID, mean, and sd
-mean_and_std <- (grepl("activityID", colNames) |
-                   grepl("subjectID", colNames) |
+mean_and_std <- (grepl("activityid", colNames) |
+                   grepl("subjectid", colNames) |
                    grepl("mean..", colNames) |
                    grepl("std...", colNames)
 )
@@ -55,12 +55,12 @@ setforMeanandStd <- finaldataset[ , mean_and_std == TRUE]
 
 #Use descriptive activity names
 setWithActivityNames <- merge(setforMeanandStd, activityLabels,
-                              by = "activityID",
+                              by = "activityid",
                               all.x = TRUE)
 
 #Make second tidy data set
-tidySet <- aggregate(. ~subjectID + activityID, setWithActivityNames, mean)
-tidySet <- tidySet[order(tidySet$subjectID, tidySet$activityID), ]
+tidySet <- aggregate(. ~ subjectid + activityid, setforMeanandStd, mean)
+tidySet <- tidySet[order(tidySet$subjectid, tidySet$activityid), ]
 
 #Write second tidy data set into a txt file
 write.table(tidySet, "tidySet.txt", row.names = FALSE)
